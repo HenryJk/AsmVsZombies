0fddecdd17f193406bf28cfd1c5c0431e56c5f6e
unknown <hsmgj2010@gmail.com>
2021-07-02 20:50:25 +0800

    add MODULE option for avz target

---
+++ cmake-build-debug/CMakeFiles/injector.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../bin/injector.exe"
+  "../bin/injector.exe.manifest"
+  "../bin/injector.pdb"
+  "../bin/libinjector.dll.a"
+  "CMakeFiles/injector.dir/injector/src/injector.cpp.obj"
+  "CMakeFiles/injector.dir/injector/src/process.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/injector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
