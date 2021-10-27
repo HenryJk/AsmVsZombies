b2bbeb908b20814cd406c912156fd4e2fc7eb991
unknown <hsmgj2010@gmail.com>
2021-10-27 09:02:48 +0800

    test library

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -31,187 +31,37 @@ namespace {
         }
         return CallWindowProc(ScWndProc, hwnd, uMsg, wParam, lParam);
     }
+
+    template<typename T>
+    void _debug_print(T t) {
+        cout << t << ");" << endl;
+    }
+
+    template<typename T, typename... Args>
+    void _debug_print(T t, Args... params) {
+        cout << t << ", ";
+        _debug_print(params...);
+    }
+
+    template<typename... Args>
+    void debug_print(const string &func, Args... params) {
+        cout << func << "(";
+        _debug_print(params...);
+    }
 } // namespace
 
-void update(pvz::Event *event) {}
+void update(Event *event) {}
 
-void DrawHealthBar(pvz::Event *event) {
-    auto app        = LawnApp::GetInstance();
-    auto d3d_device = app.mDDInterface->mD3DInterface->mD3DDevice;
-    auto &board     = *(Board *)event->mParams[0];
-    auto &plants    = board.mPlants;
-    auto &zombies   = board.mZombies;
+void TestBoardKillAllPlantsInRadius(Event *event) {
+    debug_print("Board::KillAllPlantsInRadius", (void *) event->mParams[0], event->mParams[1], event->mParams[2]);
+}
 
-    uint32_t black     = 0xFF000000;
-    uint32_t dark_gray = 0xFF303030;
-    uint32_t soft_blue = 0xFF008FC7;
-    uint32_t soft_red  = 0xFFA10B0B;
-
-    vector<D3DRECT> health_bar_containers;
-    vector<D3DRECT> health_bar;
-    if (plant_health_style) {
-        for (int i = 0; i < plants.mMaxUsedCount; i++) {
-            if ((plants.mBlock[i].mID >> 16) == 0) continue;
-            auto &plant = plants.mBlock[i].mItem;
-            if (plant.mDead) continue;
-            D3DRECT container_rectangle, health_rectangle, missing_rectangle;
-            int32_t rectangle_length;
-            uint8_t health_color[4];
-            if (plant_health_style == 1) {
-                health_color[0] = 0;
-                health_color[1] = (uint8_t) (255.0 * sqrt((float) plant.mPlantHealth / plant.mPlantMaxHealth));
-                health_color[2] = (uint8_t) (255.0 * sqrt(1.0 - ((float) plant.mPlantHealth / plant.mPlantMaxHealth)));
-                health_color[3] = 0xFF;
-            } else {
-                (uint32_t &) health_color[0] = soft_blue;
-            }
-            switch (plant.mSeedType) {
-                case SeedType::kPumpkinshell:
-                    rectangle_length    = 60 * plant.mPlantHealth / plant.mPlantMaxHealth;
-                    container_rectangle = {
-                            max(plant.mX + board.mX + 9, 0),
-                            plant.mY + board.mY - 6,
-                            plant.mX + board.mX + 71,
-                            plant.mY + board.mY - 1,
-                    };
-                    health_rectangle = {
-                            max(plant.mX + board.mX + 10, 0),
-                            plant.mY + board.mY - 5,
-                            plant.mX + board.mX + rectangle_length + 10,
-                            plant.mY + board.mY - 2,
-                    };
-                    missing_rectangle = {
-                            max(plant.mX + board.mX + rectangle_length + 10, 0),
-                            plant.mY + board.mY - 5,
-                            plant.mX + board.mX + 70,
-                            plant.mY + board.mY - 2,
-                    };
-                    break;
-                case SeedType::kLilypad:
-                case SeedType::kFlowerpot:
-                    rectangle_length    = 60 * plant.mPlantHealth / plant.mPlantMaxHealth;
-                    container_rectangle = {
-                            max(plant.mX + board.mX + 9, 0),
-                            plant.mY + board.mY + 4,
-                            plant.mX + board.mX + 71,
-                            plant.mY + board.mY + 9,
-                    };
-                    health_rectangle = {
-                            max(plant.mX + board.mX + 10, 0),
-                            plant.mY + board.mY + 5,
-                            plant.mX + board.mX + rectangle_length + 10,
-                            plant.mY + board.mY + 8,
-                    };
-                    missing_rectangle = {
-                            max(plant.mX + board.mX + rectangle_length + 10, 0),
-                            plant.mY + board.mY + 5,
-                            plant.mX + board.mX + 70,
-                            plant.mY + board.mY + 8,
-                    };
-                    break;
-                case SeedType::kCobcannon:
-                    rectangle_length    = 140 * plant.mPlantHealth / plant.mPlantMaxHealth;
-                    container_rectangle = {
-                            max(plant.mX + board.mX + 9, 0),
-                            plant.mY + board.mY - 1,
-                            plant.mX + board.mX + 151,
-                            plant.mY + board.mY + 4,
-                    };
-                    health_rectangle = {
-                            max(plant.mX + board.mX + 10, 0),
-                            plant.mY + board.mY,
-                            plant.mX + board.mX + rectangle_length + 10,
-                            plant.mY + board.mY + 3,
-                    };
-                    missing_rectangle = {
-                            max(plant.mX + board.mX + rectangle_length + 10, 0),
-                            plant.mY + board.mY,
-                            plant.mX + board.mX + 150,
-                            plant.mY + board.mY + 3,
-                    };
-                    break;
-                default:
-                    rectangle_length    = 60 * plant.mPlantHealth / plant.mPlantMaxHealth;
-                    container_rectangle = {
-                            max(plant.mX + board.mX + 9, 0),
-                            plant.mY + board.mY - 1,
-                            plant.mX + board.mX + 71,
-                            plant.mY + board.mY + 4,
-                    };
-                    health_rectangle = {
-                            max(plant.mX + board.mX + 10, 0),
-                            plant.mY + board.mY,
-                            plant.mX + board.mX + rectangle_length + 10,
-                            plant.mY + board.mY + 3,
-                    };
-                    missing_rectangle = {
-                            max(plant.mX + board.mX + rectangle_length + 10, 0),
-                            plant.mY + board.mY,
-                            plant.mX + board.mX + 70,
-                            plant.mY + board.mY + 3,
-                    };
-                    break;
-            }
-            if (container_rectangle.x2 >= 0) {
-                d3d_device->Clear(1, &container_rectangle, D3DCLEAR_TARGET, black, 0.0, 0);
-            }
-            if (health_rectangle.x2 >= 0) {
-                d3d_device->Clear(1, &health_rectangle, D3DCLEAR_TARGET, *(uint32_t *) health_color, 0.0, 0);
-            }
-            if (missing_rectangle.x2 >= 0) {
-                d3d_device->Clear(1, &missing_rectangle, D3DCLEAR_TARGET, dark_gray, 0.0, 0);
-            }
-        }
-    }
-
-    if (zombie_health_style) {
-        for (int i = 0; i < zombies.mMaxUsedCount; i++) {
-            if ((zombies.mBlock[i].mID >> 16) == 0) continue;
-            auto &zombie = zombies.mBlock[i].mItem;
-            if (zombie.mDead) continue;
-            D3DRECT container_rectangle, health_rectangle, missing_rectangle;
-            uint8_t health_color[4];
-            auto current_health = zombie.mBodyHealth + zombie.mHelmHealth;
-            auto max_health     = zombie.mBodyMaxHealth + zombie.mHelmMaxHealth;
-            if (zombie_health_style == 1) {
-                health_color[0] = 0;
-                health_color[1] = (uint8_t) (255.0 * sqrt((float) current_health / max_health));
-                health_color[2] = (uint8_t) (255.0 * sqrt(1.0 - ((float) current_health / max_health)));
-                health_color[3] = 0xFF;
-            } else {
-                (uint32_t &) health_color[0] = soft_red;
-            }
-            int32_t rectangle_length = (zombie.mZombieRect.mWidth + 18) * current_health / max_health;
-
-            container_rectangle = {
-                    max(zombie.mX + zombie.mZombieRect.mX + board.mX - 10, 0),
-                    zombie.mY + zombie.mZombieRect.mY - (int32_t)zombie.mAltitude + board.mY - 5,
-                    zombie.mX + zombie.mZombieRect.mX + board.mX + zombie.mZombieRect.mWidth + 10,
-                    zombie.mY + zombie.mZombieRect.mY - (int32_t)zombie.mAltitude + board.mY,
-            };
-            health_rectangle = {
-                    max(zombie.mX + zombie.mZombieRect.mX + board.mX - 9, 0),
-                    zombie.mY + zombie.mZombieRect.mY - (int32_t)zombie.mAltitude + board.mY - 4,
-                    zombie.mX + zombie.mZombieRect.mX + board.mX - 9 + rectangle_length,
-                    zombie.mY + zombie.mZombieRect.mY - (int32_t)zombie.mAltitude + board.mY - 1,
-            };
-            missing_rectangle = {
-                    max(zombie.mX + zombie.mZombieRect.mX + board.mX - 9 + rectangle_length, 0),
-                    zombie.mY + zombie.mZombieRect.mY - (int32_t)zombie.mAltitude + board.mY - 4,
-                    zombie.mX + zombie.mZombieRect.mX + board.mX + zombie.mZombieRect.mWidth + 9,
-                    zombie.mY + zombie.mZombieRect.mY - (int32_t)zombie.mAltitude + board.mY - 1,
-            };
-            if (container_rectangle.x2 >= 0) {
-                d3d_device->Clear(1, &container_rectangle, D3DCLEAR_TARGET, black, 0.0, 0);
-            }
-            if (health_rectangle.x2 >= 0) {
-                d3d_device->Clear(1, &health_rectangle, D3DCLEAR_TARGET, *(uint32_t *) health_color, 0.0, 0);
-            }
-            if (missing_rectangle.x2 >= 0) {
-                d3d_device->Clear(1, &missing_rectangle, D3DCLEAR_TARGET, dark_gray, 0.0, 0);
-            }
-        }
-    }
+void TestPlantFire(Event *event) {
+    debug_print("Plant::Fire",
+                (void *) event->mParams[0],
+                (void *) event->mParams[1],
+                event->mParams[2],
+                event->mParams[3]);
 }
 
 BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved) {
@@ -220,15 +70,14 @@ BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved) {
         case DLL_PROCESS_ATTACH:
             //pvz::RegisterOnTickHook(shoot);
             //            Event::InjectEventHandler(EventType::kBoard_UpdateGameObjects, update);
-            Event::InjectEventHandler(EventType::kBoard_DrawGameObjects, nullptr, DrawHealthBar);
-
-            ScWndProc = (LRESULT(CALLBACK *)(HWND, UINT, WPARAM, LPARAM)) SetWindowLongPtr(
-                    LawnApp::GetInstance().mHWnd, GWL_WNDPROC, (long) NewWndProc);
-
+            //            ScWndProc = (LRESULT(CALLBACK *)(HWND, UINT, WPARAM, LPARAM)) SetWindowLongPtr(
+            //                    LawnApp::GetInstance().mHWnd, GWL_WNDPROC, (long) NewWndProc);
             //cout << sizeof(string) << endl;
+
+            Event::InjectEventHandler(EventType::kBoard_KillAllPlantsInRadius, nullptr, TestBoardKillAllPlantsInRadius);
+            Event::InjectEventHandler(EventType::kPlant_Fire, nullptr, TestPlantFire);
             break;
         case DLL_PROCESS_DETACH:
-
             break;
         default:
             return FALSE;
